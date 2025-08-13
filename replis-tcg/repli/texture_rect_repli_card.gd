72ec0eec6b94d62ab57b7969542a44cb17cb3141
KaizNike <62921588+KaizNike@users.noreply.github.com>
2025-08-13 18:25:35 -0500

    V2 Card Madness Sweeps the Table
    Added a two more players and a play area.. Now the cards rotate and things that were going well aren't. FUN!

--- replis-tcg/repli/texture_rect_repli_card.gd
+++ replis-tcg/repli/texture_rect_repli_card.gd
@@ -1,11 +1,22 @@
 extends TextureRect
 class_name RepliCardTex
 
+var tween : Tween
+var rot_tween : Tween
+
+var old_z_index = 0
 
 func _on_mouse_entered() -> void:
-	var tween : Tween
+
 	if tween:
 		tween.kill()
+	if rot_tween:
+		rot_tween.kill()
+	rot_tween = create_tween()
+	var target_rotation = -get_parent().get_parent().rotation
+	rot_tween.tween_property(self, "rotation", target_rotation, 0.3).as_relative().set_trans(Tween.TRANS_QUAD)#.set_ease(Tween.EASE_OUT)
+	await rot_tween.finished
+	rot_tween.kill()
 	tween = create_tween()
 	tween.tween_property(self, "scale", Vector2(1.4, 1.4), 0.3).as_relative().set_trans(Tween.TRANS_ELASTIC)
 	await tween.finished
@@ -17,12 +28,15 @@ func _on_mouse_entered() -> void:
 
 	new_pos.x = clamp(new_pos.x, 0, visible_size.x - size.x * scale.x)
 	new_pos.y = clamp(new_pos.y, 0, visible_size.y - size.y * scale.y)
-
+	old_z_index = z_index
+	z_index = 100
 	global_position = new_pos
+	print(global_position)
 
 
 func _on_mouse_exited() -> void:
-	var tween : Tween
+	rotation = 0
+	z_index = old_z_index
 	if tween:
 		tween.kill()
 	tween = create_tween()
@@ -30,3 +44,9 @@ func _on_mouse_exited() -> void:
 	await tween.finished
 	position = Vector2(0, 0)
 	top_level = false
+
+
+#func _draw():
+	#var bounds = size
+	#draw_rect(Rect2(position,size), Color(1, 0, 0, 0.3), false)
+	#draw_rect(get_rect(),Color.YELLOW_GREEN,false)
